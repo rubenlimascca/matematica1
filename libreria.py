fe00a97b6a0807edaab3a10e584bf4702d32a15b
Ruben <rlimasccac@unsa.edu.pe>
2025-10-11 11:38:02 -0500

    version2

--- libreria.py
+++ libreria.py
@@ -9,3 +9,23 @@ def operacionFila(A,fil_m,fil_piv,factor): #fil_m=fil_m-factor*fil_piv
     
     def reescalaFila(A,fil_m,factor):
         A[fil_m,:]=factor*A[fil_m,:]
+        
+        
+def escalonaSimple(A):
+    nfil=A.shape[0]
+    ncol=A.shape[1]
+    
+    for j in range(0,nfil):
+        for i in range(j+1,nfil):
+            ratio=A[i,j]/A[j,j]
+            operacionFila(A,i,j,ratio)
+            
+            
+def escalonaConPiv(A):
+    nfil=A.shape[0]
+    ncol=A.shape[1]
+    for j in range(0,nfil):
+        imax=np.argmax(np.abs(A[j:nfil,j]))
+        intercambiaFilas(A,j+imax,j)
+        for i in range(j+1,nfil):
+
